2eb4f9541ce8a16eb9735c97edd3f225e19de9a3
Szabolcs Toth <szabolcs.toth@bitrise.io>
2023-06-29 14:32:53 +0100

    first commit

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Crypto",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "SPMStatic", type: .static, targets: ["Core"]),
+        .library(
+            name: "SPM",
+            targets: ["SPM"]),
+        .library(name: "SPMDynamic", type: .dynamic, targets: ["SPM"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Core",
+            dependencies: []),
+        .testTarget(
+            name: "CoreTests",
+            dependencies: ["Core"]),
+        .testTarget(
+            name: "libSPMTests",
+            dependencies: ["SPM"]),
+        .target(
+            name: "SPM",
+            dependencies: []),
+        .testTarget(
+            name: "SPMTests",
+            dependencies: ["SPM"]),
+        .testTarget(
+            name: "SPMAllTests",
+            dependencies: ["SPM"]),
+        .testTarget(
+            name: "AllTests",
+            dependencies: ["Core"]),
+        .testTarget(
+            name: "SPMStaticTests",
+            dependencies: ["Core"]),
+    ]
+)
