63c275a8a875753f1fc6fb9a051dc506322101e2
Szabolcs Toth <54896607+tothszabi@users.noreply.github.com>
2023-06-29 16:24:24 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Crypto",
+    name: "CoolFeature",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "SPMStatic", type: .static, targets: ["Core"]),
